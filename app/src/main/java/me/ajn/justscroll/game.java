59f7c70b995165014e3f59ae22104b5b55a95176
Anthony Nixon <nixon069@umn.edu>
2015-04-16 16:49:27 -0500

    Game mechanic works

--- app/src/main/java/me/ajn/justscroll/game.java
+++ app/src/main/java/me/ajn/justscroll/game.java
@@ -1,17 +1,45 @@
 package me.ajn.justscroll;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewConfiguration;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 public class game extends ActionBarActivity {
+
+    ArrayList<String> elementsArray = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        ListView myListView = (ListView) findViewById(R.id.listView);
+        //ArrayList<String> elementsArray = new ArrayList<String>();
+        elementsArray.add("");
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, elementsArray);;
+        myListView.setAdapter(adapter);
+
+        myListView.setFriction(ViewConfiguration.getScrollFriction() * 2);
+
+        final TextView score = (TextView)findViewById(R.id.score);
+
+        myListView.setOnScrollListener(new InfiniteScrollListener(5) {
+            @Override
+            public void loadMore(int page, int totalItemsCount) {
+                //List<HashMap<String, String>> newData = loader.loadData();
+                elementsArray.add("");
+                score.setText("Score: " + Integer.toString(page/100));
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 
 
